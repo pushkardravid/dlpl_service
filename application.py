f93a2fccf683bac441361b40611ed7d86972bbf1
Pushkar <pushkarsdravid@gmail.com>
2019-02-23 02:34:55 +0530

    teams endpoints added

--- application.py
+++ application.py
@@ -13,8 +13,6 @@ def encode(obj):
 	for key, value in obj.items():
 		if isinstance(value, ObjectId):
 			obj[key] = str(value)
-		elif isinstance(value, list):
-			obj[key] = [str(i) for i in value]
 	return obj
 
 @app.route("/players/<id>", methods=['GET'])
@@ -33,6 +31,16 @@ def get_all_players():
 	else:
 		players = mongo.db.player_statistics.find()
 	return jsonify({"response":[encode(player) for player in players]})
+
+@app.route("/teams/<id>", methods=['GET'])
+def get_team(id):
+	teams = mongo.db.teams.find({'id':int(id)})
+	return jsonify({"response":[encode(team) for team in teams]})
+
+@app.route("/teams", methods=['GET'])
+def get_all_teams():
+	teams = mongo.db.teams.find().sort('points',-1)
+	return jsonify({"response":[team for team in teams]})
 
 if __name__ == '__main__':
     app.run(debug=True)
